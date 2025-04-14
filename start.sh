434fe394e08b04ab962b8c4c3c5e228067500d4a
rafet289 <79151705+rafet289@users.noreply.github.com>
2025-04-14 13:41:19 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-echo -e '#!/bin/bash\necho "Starting futbolazapi app..."\nexec uvicorn main:app --host=0.0.0.0 --port=${PORT:-8000}' > start.sh
+echo -e '#!/bin/bash\necho "Starting main app..."\nexec uvicorn main:app --host=0.0.0.0 --port=${PORT:-8000}' > start.sh
 chmod +x start.sh
